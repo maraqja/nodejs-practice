7f93017de9b0cc6e881b0e7687a6a69c0a2da757
Dmitriy Morkulev <morkulev.dim@yandex.ru>
2023-06-15 14:40:50 +0300

    validation middleware
    добавлен middleware для валидации

--- src/common/route.interface.ts
+++ src/common/route.interface.ts
@@ -1,10 +1,11 @@
 import { NextFunction, Request, Response, Router } from 'express';
+import { IMiddleware } from './middleware.interface';
 
 export interface IControllerRoute {
 	path: string;
 	function: (req: Request, res: Response, next: NextFunction) => void;
 	method: keyof Pick<Router, 'get' | 'post' | 'delete' | 'put' | 'patch'>; // Pick берет только некоторые поля и создает из них новый интерфейс
-	middlewares?: IMiddleWare[];
+	middlewares?: IMiddleware[];
 
 	// method: 'get' | 'post' | 'delete' | 'put' | 'patch'
 }
--- src/users/dto/user-register.dto.ts
+++ src/users/dto/user-register.dto.ts
@@ -1,5 +1,12 @@
+import { IsEmail, IsString } from 'class-validator';
+
 export class UserRegisterDto {
+	@IsEmail({}, { message: 'Не соответствует формату e-mail' })
 	email: string;
+
+	@IsString({ message: 'Не указан пароль' })
 	password: string;
+
+	@IsString({ message: 'Не указано имя' })
 	name: string;
 }
